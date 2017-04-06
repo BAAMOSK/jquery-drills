4aafc6dab4edb08a26723ed802118b34a2011733
BAAMOSK <bunth667@gmail.com>
2017-04-06 17:30:05 -0400

    turned light bulbs on and off

---
+++ lightBulbs.js
@@ -0,0 +1,11 @@
+var lightBulbs = $('.lightbulb');
+
+
+
+lightBulbs.click(function(event) {
+    
+//    $(event.currentTarget).toggleClass('bulb-on');
+    $('.lightbulb').removeClass('bulb-on');
+    $(event.currentTarget).addClass('bulb-on');
+    
+});
